66bbb1224ed9ee3adf142b60a36295dc5f4c6fde
Stanislav Vitko <stas@vitko.me>
2019-11-20 09:42:59 -0800

    fix tests

--- union-find/weighteduf_test.go
+++ union-find/weighteduf_test.go
@@ -9,7 +9,7 @@ func TestQuickUnionWUF_Connected(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			uf := NewQuickUnionUF(tt.n)
+			uf := NewQuickUnionWUF(tt.n)
 			for _, union := range tt.unions {
 				uf.Union(union.p, union.q)
 			}
@@ -27,8 +27,8 @@ func TestQuickUnionWUF_Connected(t *testing.T) {
 }
 
 func benchmarkQuickUnionWUF_Union(size int, b *testing.B) {
-	// create uf with b.N comonents
-	uf := NewQuickUnionUF(size)
+	// create uf with b.N components
+	uf := NewQuickUnionWUF(size)
 	// run union b.N times on random components
 	for i := 0; i < b.N; i++ {
 		p := rand.Intn(size)
@@ -39,11 +39,10 @@ func benchmarkQuickUnionWUF_Union(size int, b *testing.B) {
 
 // benchmark QuickFindUF_Union with different inputs
 
-func BenchmarkQuickUnionWUF_Union1(b *testing.B)        { benchmarkQuickUnionWUF_Union(1, b) }
-func BenchmarkQuickUnionWUF_Union10(b *testing.B)       { benchmarkQuickUnionWUF_Union(10, b) }
-func BenchmarkQuickUnionWUF_Union100(b *testing.B)      { benchmarkQuickUnionWUF_Union(100, b) }
-func BenchmarkQuickUnionWUF_Union1000(b *testing.B)     { benchmarkQuickUnionWUF_Union(1000, b) }
-func BenchmarkQuickUnionWUF_Union10000(b *testing.B)    { benchmarkQuickUnionWUF_Union(10000, b) }
-func BenchmarkQuickUnionWUF_Union100000(b *testing.B)   { benchmarkQuickUnionWUF_Union(100000, b) }
-func BenchmarkQuickUnionWUF_Union1000000(b *testing.B)  { benchmarkQuickUnionWUF_Union(1000000, b) }
-func BenchmarkQuickUnionWUF_Union10000000(b *testing.B) { benchmarkQuickUnionWUF_Union(10000000, b) }
+func BenchmarkQuickUnionWUF_Union1(b *testing.B)     { benchmarkQuickUnionWUF_Union(1, b) }
+func BenchmarkQuickUnionWUF_Union10(b *testing.B)    { benchmarkQuickUnionWUF_Union(10, b) }
+func BenchmarkQuickUnionWUF_Union100(b *testing.B)   { benchmarkQuickUnionWUF_Union(100, b) }
+func BenchmarkQuickUnionWUF_Union1000(b *testing.B)  { benchmarkQuickUnionWUF_Union(1000, b) }
+func BenchmarkQuickUnionWUF_Union10000(b *testing.B) { benchmarkQuickUnionWUF_Union(10000, b) }
+
+//func BenchmarkQuickUnionWUF_Union100000(b *testing.B) { benchmarkQuickUnionWUF_Union(100000, b) }
