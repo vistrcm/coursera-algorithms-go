2f15ee63499b80f137cf397e1d3ef9abcc810f4f
Stanislav Vitko <stas@vitko.me>
2020-01-04 12:09:12 -0800

    implement queue via linked list

---
+++ queue/queue.go
@@ -0,0 +1,56 @@
+package queue
+
+type OfStrings interface {
+	//Enqueue inserts an item into the queue
+	Enqueue(item string)
+	//Dequeue removes and returns item least recently added
+	Dequeue() string
+	//IsEmpty check if the queue is empty
+	IsEmpty() bool
+}
+
+type node struct {
+	item string
+	next *node
+}
+
+//OfStringsLL linked list implementation of queue
+type OfStringsLL struct {
+	first *node
+	last  *node
+}
+
+//NewOfStringsLL create new instance of OfStringsLL
+func NewOfStringsLL() *OfStringsLL {
+	return &OfStringsLL{}
+}
+
+//Enqueue inserts an item into the queue
+func (q *OfStringsLL) Enqueue(item string) {
+	oldLast := q.last
+	//new node for the end of the list
+	last := &node{
+		item: item,
+		next: nil,
+	}
+	if q.IsEmpty() {
+		q.first = q.last
+	} else {
+		oldLast.next = last
+	}
+}
+
+//Dequeue removes and returns item least recently aded
+func (q *OfStringsLL) Dequeue() string {
+	item := q.first.item
+	q.first = q.first.next
+	if q.IsEmpty() {
+		q.last = nil
+	}
+	return item
+}
+
+//IsEmpty check if the queue is empty
+func (q *OfStringsLL) IsEmpty() bool {
+	return q.first == nil
+}
