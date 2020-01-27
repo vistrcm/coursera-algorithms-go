c912b1a98a986290bfa8bc0c8b53fb2045c51903
Stanislav Vitko <stas@vitko.me>
2020-01-26 17:05:31 -0800

    fmt sort tests

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -155,20 +155,20 @@ func benchmarkInsertionDescending(size int, b *testing.B) {
 	benchmarkSearchDescending(Insertion, size, b)
 }
 
-func BenchmarkInsertion_1(b *testing.B)    { benchmarkInsertion(1, b) }
-func BenchmarkInsertion_10(b *testing.B)   { benchmarkInsertion(10, b) }
-func BenchmarkInsertion_100(b *testing.B)  { benchmarkInsertion(100, b) }
-func BenchmarkInsertion_1000(b *testing.B) { benchmarkInsertion(1000, b) }
+func BenchmarkInsertion_1(b *testing.B)     { benchmarkInsertion(1, b) }
+func BenchmarkInsertion_10(b *testing.B)    { benchmarkInsertion(10, b) }
+func BenchmarkInsertion_100(b *testing.B)   { benchmarkInsertion(100, b) }
+func BenchmarkInsertion_1000(b *testing.B)  { benchmarkInsertion(1000, b) }
 func BenchmarkInsertion_10000(b *testing.B) { benchmarkInsertion(10000, b) }
 
-func BenchmarkInsertionSorted_1(b *testing.B)    { benchmarkInsertionSorted(1, b) }
-func BenchmarkInsertionSorted_10(b *testing.B)   { benchmarkInsertionSorted(10, b) }
-func BenchmarkInsertionSorted_100(b *testing.B)  { benchmarkInsertionSorted(100, b) }
-func BenchmarkInsertionSorted_1000(b *testing.B) { benchmarkInsertionSorted(1000, b) }
+func BenchmarkInsertionSorted_1(b *testing.B)     { benchmarkInsertionSorted(1, b) }
+func BenchmarkInsertionSorted_10(b *testing.B)    { benchmarkInsertionSorted(10, b) }
+func BenchmarkInsertionSorted_100(b *testing.B)   { benchmarkInsertionSorted(100, b) }
+func BenchmarkInsertionSorted_1000(b *testing.B)  { benchmarkInsertionSorted(1000, b) }
 func BenchmarkInsertionSorted_10000(b *testing.B) { benchmarkInsertionSorted(10000, b) }
 
-func BenchmarkInsertionDescending_1(b *testing.B)    { benchmarkInsertionDescending(1, b) }
-func BenchmarkInsertionDescending_10(b *testing.B)   { benchmarkInsertionDescending(10, b) }
-func BenchmarkInsertionDescending_100(b *testing.B)  { benchmarkInsertionDescending(100, b) }
-func BenchmarkInsertionDescending_1000(b *testing.B) { benchmarkInsertionDescending(1000, b) }
+func BenchmarkInsertionDescending_1(b *testing.B)     { benchmarkInsertionDescending(1, b) }
+func BenchmarkInsertionDescending_10(b *testing.B)    { benchmarkInsertionDescending(10, b) }
+func BenchmarkInsertionDescending_100(b *testing.B)   { benchmarkInsertionDescending(100, b) }
+func BenchmarkInsertionDescending_1000(b *testing.B)  { benchmarkInsertionDescending(1000, b) }
 func BenchmarkInsertionDescending_10000(b *testing.B) { benchmarkInsertionDescending(10000, b) }
