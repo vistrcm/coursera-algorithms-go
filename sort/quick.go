7f160603610e97cfd8fcc8e5924250fa3d05fcd9
Stanislav Vitko <stas@vitko.me>
2020-02-04 21:30:11 -0800

    add quicksort implementation

---
+++ sort/quick.go
@@ -0,0 +1,39 @@
+package sort
+
+import (
+	"math/rand"
+	"sort"
+)
+
+//Quick implements quicksort algorithm
+func Quick(a sort.IntSlice) {
+	rand.Shuffle(a.Len(), a.Swap)
+	qIntSort(a, 0, a.Len() -1 )
+}
+
+func qIntSort(a sort.IntSlice, lo, hi int) {
+	if hi <= lo {return}
+	j := partition(a, lo, hi)
+	qIntSort(a, lo, j-1)
+	qIntSort(a, j+1, hi)
+}
+
+func partition(a sort.IntSlice, lo, hi int) int {
+	i := lo
+	j := hi +1
+	for {
+		for i++;a.Less(i, lo); i++ { // find item on the left to swap
+			if i == hi {break}
+		}
+
+		for j--; a.Less(lo, j); j-- { // find item on the right to swap
+			if j == lo {break}
+		}
+
+		if i>= j {break}
+
+		a.Swap(i, j)
+	}
+	a.Swap(lo, j)
+	return j
+}
