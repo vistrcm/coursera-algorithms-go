03d19a823d4a2849fb31a5403aa266417bcf1980
Stanislav Vitko <stas@vitko.me>
2020-02-08 15:58:27 -0800

    priorityQueue. norderedMaxPQ_test.go -> heap_test.go

--- priorityQueue/heap_test.go
+++ priorityQueue/heap_test.go
@@ -25,3 +25,26 @@ func ExampleUnorderedMaxPQ() {
 	// X
 	// P
 }
+
+
+
+func ExampleHeapPQ() {
+	q := priorityQueue.NewHeapPQ(7) // need at least 7 positions to store next elements
+	q.Insert(priorityQueue.Item{"P"})
+	q.Insert(priorityQueue.Item{"Q"})
+	q.Insert(priorityQueue.Item{"E"})
+	fmt.Println(q.DelMax().Value) //Q
+
+	q.Insert(priorityQueue.Item{"X"})
+	q.Insert(priorityQueue.Item{"A"})
+	q.Insert(priorityQueue.Item{"M"})
+	fmt.Println(q.DelMax().Value) //X
+
+	q.Insert(priorityQueue.Item{"P"})
+	q.Insert(priorityQueue.Item{"L"})
+	q.Insert(priorityQueue.Item{"E"})
+	fmt.Println(q.DelMax().Value) //P
+	// Output: Q
+	// X
+	// P
+}
