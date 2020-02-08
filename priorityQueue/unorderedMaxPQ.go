287196c3caa383bce217bbf77e0f7d3c250a2c3b
Stanislav Vitko <stas@vitko.me>
2020-02-08 13:34:12 -0800

    fix unorderedMaxPQ

--- priorityQueue/unorderedMaxPQ.go
+++ priorityQueue/unorderedMaxPQ.go
@@ -9,41 +9,48 @@ type Item struct {
 }
 
 //UnorderedMaxPQ simple implementation of a Priority queue
-type UnorderedMaxPQ []*Item
+type UnorderedMaxPQ struct {
+	items []*Item
+	N     int
+}
 
 //NewUnorderedMaxPQ create new instance of UnorderedMaxPQ
-func NewUnorderedMaxPQ(capacity int) *UnorderedMaxPQ{
-	pq := make(UnorderedMaxPQ, 0, capacity)
-	return &pq
+func NewUnorderedMaxPQ(capacity int) *UnorderedMaxPQ {
+	items := make([]*Item, capacity)
+	return &UnorderedMaxPQ{items: items}
 }
 
 //IsEmpty is true if no more elements in the UnorderedMaxPQ
-func (pq UnorderedMaxPQ)IsEmpty() bool {
-	return len(pq) == 0
+func (pq UnorderedMaxPQ) IsEmpty() bool {
+	return pq.N == 0
 }
 
 //Insert key to the UnorderedMaxPQ
-func (pq *UnorderedMaxPQ)Insert(key Item) {
-	*pq = append(*pq, &key)
+func (pq *UnorderedMaxPQ) Insert(key Item) {
+	pq.items[pq.N] = &key
+	pq.N++
+
 }
 
-func (pq UnorderedMaxPQ)less(a, b int) bool{
-	return pq[a].Value < pq[b].Value
+func (pq UnorderedMaxPQ) less(a, b int) bool {
+	return pq.items[a].Value < pq.items[b].Value
 }
 
-func (pq UnorderedMaxPQ)swap(a, b int){
-	pq[a], pq[b] = pq[b], pq[a]
+func (pq UnorderedMaxPQ) swap(a, b int) {
+	pq.items[a], pq.items[b] = pq.items[b], pq.items[a]
 }
 
 //DelMax delete's maximum Item
-func (pq *UnorderedMaxPQ)DelMax() Item {
+func (pq *UnorderedMaxPQ) DelMax() Item {
 	max := 0
-	for i := 1; i< len(*pq); i++ {
-		if pq.less(max, i) {max = i}
+	for i := 1; i < pq.N; i++ {
+		if pq.less(max, i) {
+			max = i
+		}
 	}
-	pq.swap(max, len(*pq) - 1)
-	old := *pq
-	item := old[len(old)-1]
-	*pq = old[0: len(old)-1]
+	pq.swap(max, pq.N-1)
+
+	pq.N--
+	item := pq.items[pq.N]
 	return *item
 }
