91795f7f7f52220449edd468cf2bd23f4eac469f
Stanislav Vitko <stas@vitko.me>
2020-02-15 23:54:10 -0800

    symbol tree example

---
+++ st/st_test.go
@@ -0,0 +1,41 @@
+package st_test
+
+import (
+	"fmt"
+	"github.com/vistrcm/coursera-algorithms-pi-go/st"
+)
+
+type comparableString string
+
+func (s comparableString) CompareTo(o interface{}) int {
+	other := o.(comparableString)
+	if s < other {
+		return -1
+	} else if s > other {
+		return +1
+	}
+	return 0
+}
+
+func ExampleBST() {
+	keys := []string{"S", "E", "A", "R", "C", "H", "E", "X", "A", "M", "P", "L", "E"}
+	stable := st.BST{}
+
+	for i, key := range keys {
+		stable.Put(comparableString(key), i)
+	}
+
+	for _, k := range stable.Keys() {
+		fmt.Printf("%s -> %d\n", k, stable.Get(k))
+	}
+	// Output: A -> 8
+	// C -> 4
+	// E -> 12
+	// H -> 5
+	// L -> 11
+	// M -> 9
+	// P -> 10
+	// R -> 3
+	// S -> 0
+	// X -> 7
+}
