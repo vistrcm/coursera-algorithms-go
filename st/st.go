002403ccb88b91ea992657403aad75a124d33984
Stanislav Vitko <stas@vitko.me>
2020-02-15 22:45:45 -0800

    continue implementation of symbol table

--- st/st.go
+++ st/st.go
@@ -6,7 +6,7 @@ type Comparable interface {
 }
 
 type Key Comparable
-type Value string
+type Value interface{}
 
 type Node struct {
 	key   Key
@@ -21,26 +21,46 @@ func NewNode(key Key, value Value) *Node {
 
 type BST struct {
 	root *Node
-
 }
 
-func (bst BST)Put(key Key, value Value){
-	panic("NOT implemented")
+func (bst BST) Put(key Key, value Value) {
+	bst.root = put(bst.root, key, value)
 }
 
-func (bst BST)Get(key Key) Value{
-	x := bst.root
-	for x!=nil {
-		cmp := key.CompareTo(x.key)
-		if cmp <0 {x = x.left}
-		else if cmp > 0 {x}
+func put(x *Node, key Key, value Value) *Node {
+	if x == nil {
+		return NewNode(key, value)
 	}
+	cmp := key.CompareTo(x.key)
+	if cmp < 0 {
+		x.left = put(x.left, key, value)
+	} else if cmp > 0 {
+		x.right = put(x.right, key, value)
+	} else {
+		x.value = value
+	}
+	return x
 }
 
-func (bst BST)Delete(key Key){
+func (bst BST) Get(key Key) Value {
+	x := bst.root
+	for x != nil {
+		cmp := key.CompareTo(x.key)
+		if cmp < 0 {
+			x = x.left
+		} else if cmp > 0 {
+			x = x.right
+		} else {
+			return x.value
+		}
+	}
+	return nil
+}
+
+func (bst BST) Delete(key Key) {
 	panic("NOT implemented")
 }
 
-func (bst BST)Keys() []Key{
+func (bst BST) Keys() []Key {
 	panic("NOT implemented")
 }
