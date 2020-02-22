75fbc7c1511ed07df3f4472d6680400f22740039
Stanislav Vitko <stas@vitko.me>
2020-02-22 09:40:04 -0800

    first working version of red-black BST. example

--- st/st_test.go
+++ st/st_test.go
@@ -3,6 +3,7 @@ package st_test
 import (
 	"fmt"
 	"github.com/vistrcm/coursera-algorithms-pi-go/st"
+	"github.com/vistrcm/coursera-algorithms-pi-go/st/rbbst"
 )
 
 type comparableString string
@@ -20,6 +21,29 @@ func (s comparableString) CompareTo(o interface{}) int {
 func ExampleBST() {
 	keys := []string{"S", "E", "A", "R", "C", "H", "E", "X", "A", "M", "P", "L", "E"}
 	stable := st.BST{}
+
+	for i, key := range keys {
+		stable.Put(comparableString(key), i)
+	}
+
+	for _, k := range stable.Keys() {
+		fmt.Printf("%s -> %d\n", k, stable.Get(k))
+	}
+	// Output: A -> 8
+	// C -> 4
+	// E -> 12
+	// H -> 5
+	// L -> 11
+	// M -> 9
+	// P -> 10
+	// R -> 3
+	// S -> 0
+	// X -> 7
+}
+
+func ExampleRBBST() {
+	keys := []string{"S", "E", "A", "R", "C", "H", "E", "X", "A", "M", "P", "L", "E"}
+	stable := rbbst.RBBST{}
 
 	for i, key := range keys {
 		stable.Put(comparableString(key), i)
