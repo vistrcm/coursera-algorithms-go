7b71387b1feb6b610d25b4c5d8a3db4544ebfabd
Stanislav Vitko <stas@vitko.me>
2020-03-08 00:55:32 -0800

    extend graph interface

--- graph/graph.go
+++ graph/graph.go
@@ -3,10 +3,13 @@ package graph
 type Interface interface {
 	AddEdge(v, w int)
 	Adj(v int) []int
+	V() int
+	E() int
 }
 
 type Graph struct {
 	v   int     // amount of vertices
+	e   int     // amount of egdes
 	adj [][]int // adjacency list
 }
 
@@ -21,8 +24,17 @@ func NewGraph(v int) Graph {
 func (g *Graph) AddEdge(v, w int) {
 	g.adj[v] = append(g.adj[v], w)
 	g.adj[w] = append(g.adj[w], v)
+	g.e++
 }
 
 func (g *Graph) Adj(v int) []int {
 	return g.adj[v]
+}
+
+func (g *Graph) V() int {
+	return g.v
+}
+
+func (g *Graph) E() int {
+	return g.e
 }
