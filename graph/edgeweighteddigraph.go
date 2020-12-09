6f4276c20a11bf1d59ebf9f7e15c5d12bfe5f2bb
Stanislav Vitko <stas@vitko.me>
2020-12-08 20:16:09 -0800

    edgeweighteddigraph

---
+++ graph/edgeweighteddigraph.go
@@ -0,0 +1,67 @@
+package graph
+
+import "fmt"
+
+type DirectedEdge struct {
+	v      int
+	w      int
+	weight float64
+}
+
+func NewDirectedEdge(v, w int, weight float64) DirectedEdge {
+	return DirectedEdge{
+		v:      v,
+		w:      w,
+		weight: weight,
+	}
+}
+
+func (e DirectedEdge) From() int {
+	return e.v
+}
+
+func (e DirectedEdge) To() int {
+	return e.w
+}
+
+func (e DirectedEdge) Weight() float64 {
+	return e.weight
+}
+
+func (e DirectedEdge) String() string {
+	return fmt.Sprintf("%d->%d  %.2f", e.v, e.w, e.weight)
+}
+
+
+type EdgeWeightedDigraph struct {
+	v   int
+	adj [][]DirectedEdge
+}
+
+func NewEdgeWeightedDigraph(v int) EdgeWeightedDigraph {
+	var adj = make([][]DirectedEdge, v)
+	for i, _ := range adj {
+		adj[i] = make([]DirectedEdge, 0)
+	}
+
+	return EdgeWeightedDigraph{
+		v:   v,
+		adj: adj,
+	}
+}
+
+func (g *EdgeWeightedDigraph) AddEdge(e DirectedEdge) {
+	v := e.From()
+	g.adj[v] = append(g.adj[v], e)
+}
+
+func (g *EdgeWeightedDigraph) Adj(v int) []DirectedEdge {
+	aux := make([]DirectedEdge, len(g.adj[v]))
+	copy(g.adj[v], aux)
+	return aux
+}
+
+//V return amount of vertices
+func (g *EdgeWeightedDigraph) V() int {
+	return g.v
+}
---
+++ graph/sp.go
@@ -0,0 +1,23 @@
+package graph
+
+type SP struct {
+
+}
+
+func NewSP(g EdgeWeightedDigraph, s int) SP {
+	// TODO: implement
+	panic("not implemented")
+}
+
+func (sp *SP) DistTo(v int) float64 {
+	// TODO: implement
+	panic("not implemented")
+}
+
+func (sp *SP) PathTo(v int) []DirectedEdge {
+	// TODO: implement
+	panic("not implemented")
+}
+
+
+
