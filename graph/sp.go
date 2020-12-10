79673f3d1f31225f7bc0b722511d677075ccad54
Stanislav Vitko <stas@vitko.me>
2020-12-10 09:05:49 -0800

    draft shortest path

--- graph/sp.go
+++ graph/sp.go
@@ -1,7 +1,8 @@
 package graph
 
 type SP struct {
-
+	distTo []float64
+	edgeTo []*DirectedEdge
 }
 
 func NewSP(g EdgeWeightedDigraph, s int) SP {
@@ -10,13 +11,23 @@ func NewSP(g EdgeWeightedDigraph, s int) SP {
 }
 
 func (sp *SP) DistTo(v int) float64 {
-	// TODO: implement
-	panic("not implemented")
+	return sp.distTo[v]
 }
 
-func (sp *SP) PathTo(v int) []DirectedEdge {
-	// TODO: implement
-	panic("not implemented")
+func (sp *SP) PathTo(v int) []*DirectedEdge {
+	var path []*DirectedEdge
+	for e := sp.edgeTo[v]; e != nil; e = sp.edgeTo[e.From()] {
+		path = append(path, e)
+	}
+	return path
+}
+
+func (sp *SP) relax(e DirectedEdge)  {
+	v := e.From(); w := e.To()
+	if sp.distTo[w] > sp.distTo[v] + e.weight {
+		sp.distTo[w] = sp.distTo[v] + e.weight
+		sp.edgeTo[w] = &e
+	}
 }
 
 
